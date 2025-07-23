bd3fceadf62d2882ef3758bfdc4891f0e286ccf8
Oskar Tornevall <oskar.tornevall@gmail.com>
2025-07-23 22:29:51 +0200

    initial

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+
+  outputs = { self, nixpkgs, poetry2nix }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      over = final: prev: {
+      };
+    in
+    {
+      packages = forAllSystems (system: let
+        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
+      in {
+        default = mkPoetryApplication { projectDir = self; };
+      });
+
+      devShells = forAllSystems (system: let
+        inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv overrides;
+      in {
+        default = pkgs.${system}.mkShellNoCC {
+          packages = with pkgs.${system}; [
+            (mkPoetryEnv { projectDir = self; overrides = overrides.withDefaults over; })
+            poetry
+            pyright
+          ];
+        };
+      });
+
+      nixosModules.default = {lib, config, options, pkgs, ...}:
+        with lib;
+        let inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs; }) mkPoetryApplication;
+            app = mkPoetryApplication { projectDir = self; };
+        in {
+           options.satrn.programs.viacpu = with types; {
+             enable = mkBoolOpt false "Enable minimal system configuration";
+           };
+
+           config = mkIf cfg.enable {
+             systemd.services.viacpu = {
+               wantedBy = ["graphical.target"];
+               serviceConfig = {
+                 Type = "Simple";
+                 User = "sora";
+                 Exec = "${app.out}/bin/viacpu";
+               };
+             };
+
+             environment.systemPackages = [ app ];
+           };
+        };
+    };
+}
---
+++ via_cpu/main.py
@@ -0,0 +1,51 @@
+import sys
+import hid
+import asyncio
+import psutil
+
+vendor_id     = 0x04d8
+product_id    = 0xeb2d
+
+usage_page    = 0xFF60
+usage         = 0x61
+report_length = 32
+
+def get_raw_hid_interface():
+    device_interfaces = hid.enumerate(vendor_id, product_id)
+    raw_hid_interfaces = [i for i in device_interfaces if i['usage_page'] == usage_page and i['usage'] == usage]
+
+    if len(raw_hid_interfaces) == 0:
+        return None
+
+    interface = hid.Device(path=raw_hid_interfaces[0]['path'])
+
+    return interface
+
+def send_raw_report(data):
+    interface = get_raw_hid_interface()
+
+    if interface is None:
+        print("No device found")
+        sys.exit(1)
+
+    request_data = [0x00] * (report_length + 1) # First byte is Report ID
+    request_data[1:len(data) + 1] = data
+    request_report = bytes(request_data)
+
+    try:
+        interface.write(request_report)
+        response_report = interface.read(report_length, timeout=1000)
+    finally:
+        interface.close()
+
+async def _main():
+    while True:
+        await asyncio.sleep(1)
+        cpu = int(psutil.cpu_percent())
+        mem = int(psutil.virtual_memory().percent)
+        send_raw_report([
+            0x07, cpu, mem
+        ])
+
+def main():
+    asyncio.run(_main())
